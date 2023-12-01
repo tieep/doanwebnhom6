421b0acb6f38e7759ef3861a77260478910c7e6d
tieep <151663415+tieep@users.noreply.github.com>
2023-12-01 20:12:47 +0700

    Update main..js

--- main..js
+++ main..js
@@ -19,3 +19,45 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+var homePage = document.getElementById("homePage");
+
+function homePageClick(){
+    homePage.style.display = "block";
+}
+
+function showDetail1(){
+    document.getElementById('product_detail1').style.top="10%";
+}
+function showDetail2(){
+    document.getElementById('product_detail2').style.top="10%";
+}
+function showDetail3(){
+    document.getElementById('product_detail3').style.top="10%";
+}
+function showDetail4(){
+    document.getElementById('product_detail4').style.top="10%";
+}
+function showDetail5(){
+    document.getElementById('product_detail5').style.top="10%";
+}
+function showDetail6(){
+    document.getElementById('product_detail6').style.top="10%";
+}
+function showDetail7(){
+    document.getElementById('product_detail7').style.top="10%";
+}
+function showDetail8(){
+    document.getElementById('product_detail8').style.top="10%";
+}
+// function closeDetail()
+// {
+// 	document.getElementById('product_detail1').style.top = "-300%";
+//     document.getElementById('product_detail2').style.top = "-300%";
+//     document.getElementById('product_detail3').style.top = "-300%";
+//     document.getElementById('product_detail4').style.top = "-300%";
+//     document.getElementById('product_detail5').style.top = "-300%";
+//     document.getElementById('product_detail6').style.top = "-300%";
+//     document.getElementById('product_detail7').style.top = "-300%";
+//     document.getElementById('product_detail8').style.top = "-300%";
+// }
